a97f120ef46499837173614198eaf0329d1c4947
alexandrekj <57239825+alexandrekj@users.noreply.github.com>
2020-10-07 20:14:15 -0300

    Solução do exercícío 2a
    Basicamente o que foi realizado nessa atividade foi replicar as inicializações das tasks já existentes para os outros 2 leds da placa, alterando a temporização conforme solicitado utilizando-se vTaskDelayUntil, limitando assim o período total de execução da task.

--- tasks_FreeRTOS_qemu.c
+++ tasks_FreeRTOS_qemu.c
@@ -14,19 +14,33 @@
 #include "task.h"
 
 
-void vBlinkLED4(void *pvParam){
-  const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+void vBlinkLED3(void *pvParam){ // LED 3
+  TickType_t xLastWakeTime;
+  const TickType_t xPeriod = 530 / portTICK_PERIOD_MS; // 530 ms
+  xLastWakeTime = xTaskGetTickCount();
+
+  while(1){
+    BSP_LED_Toggle(LED3);
+    vTaskDelayUntil(&xLastWakeTime, xPeriod);
+  } // while
+} // vBlinkLED3
+
+
+void vBlinkLED4(void *pvParam){ // LED 4
+  TickType_t xLastWakeTime;
+  const TickType_t xDelay = 430 / portTICK_PERIOD_MS;
+  xLastWakeTime = xTaskGetTickCount();
 
   while(1){
     BSP_LED_Toggle(LED4);
-    vTaskDelay(xDelay);
+    vTaskDelayUntil(&xLastWakeTime, xDelay); // Delay -> período total
   } // while
 } // vBlinkLED4
 
 
-void vBlinkLED5(void *pvParam){
+void vBlinkLED5(void *pvParam){ // LED 5
   TickType_t xLastWakeTime;
-  const TickType_t xPeriod = 500 / portTICK_PERIOD_MS;
+  const TickType_t xPeriod = 330 / portTICK_PERIOD_MS;
   xLastWakeTime = xTaskGetTickCount();
 
   while(1){
@@ -35,14 +49,32 @@ void vBlinkLED5(void *pvParam){
   } // while
 } // vBlinkLED5
 
+void vBlinkLED6(void *pvParam){ // LED 6
+  TickType_t xLastWakeTime;
+  const TickType_t xPeriod = 230 / portTICK_PERIOD_MS;
+  xLastWakeTime = xTaskGetTickCount();
+
+  while(1){
+    BSP_LED_Toggle(LED6);
+    vTaskDelayUntil(&xLastWakeTime, xPeriod);
+  } // while
+} // vBlinkLED6
+
+
+
 
 void main(void){
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  BSP_LED_Init(LED3);
   BSP_LED_Init(LED4);
   BSP_LED_Init(LED5);
+  BSP_LED_Init(LED6);
 
+  xTaskCreate(vBlinkLED3, "vBlinkLED3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
   xTaskCreate(vBlinkLED4, "vBlinkLED4", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
   xTaskCreate(vBlinkLED5, "vBlinkLED5", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+  xTaskCreate(vBlinkLED6, "vBlinkLED6", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+
 
   vTaskStartScheduler();
   while(1);
